ffd805e8842e14b16754b619ecda2e5d98fe7298
Pavel <19paul91@mail.ru>
2016-05-22 19:26:23 +0300

    final 2 default

---
+++ MyRestaurants/MyRestaurants/MyRestarauntsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyRestarauntsTableViewCell.swift
+//  MyRestaurants
+//
+//  Created by Павел Анплеенко on 22/05/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+
+class MyRestarauntsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
