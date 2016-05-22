0fbc4c0d214f9ba43a558b6da0b95551b57f2f7a
Pavel <19paul91@mail.ru>
2016-05-22 12:21:04 +0300

    add alert and button

--- Hello-World/Hello-World/ViewController.swift
+++ Hello-World/Hello-World/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBAction func showAllertMessege() {
+    
+    // вызов алерта с текстом
+    let alertControler = UIAlertController(title: "Hello world Detected", message: "This is my fist app", preferredStyle: UIAlertControllerStyle.Alert)
+    
+    // действие добавляет кнопку
+    alertControler.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+    alertControler.addAction(UIAlertAction(title: "Cansel", style: UIAlertActionStyle.Default, handler: nil))
+    
+    // настройки анимации true - плавная, false - резкая
+    self.presentViewController(alertControler, animated: true, completion: nil)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
