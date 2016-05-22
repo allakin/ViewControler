70cf34e468c848ecd5721d293eb2b873ba16e881
Pavel <19paul91@mail.ru>
2016-05-22 16:03:23 +0300

    add massiveName restaurant

--- simpleTableViewApp/simpleTableViewApp/ViewController.swift
+++ simpleTableViewApp/simpleTableViewApp/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+  
+  // переменная с масивов в котором уже есть рестораны по умолчанию
+  var restaurantNames = ["Ogonek", "Grill&Bar", "Елу", "Bonsai", "Дастархан", "Индокитай", "Х.О", "Балкан Гриль", "Respublica", "Speak Easy", "Moris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+  
+  // функция сколько рядом находится в одной секции
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    returt 1
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cellIndentifier = "Cell"
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIndentifier, forIndexPath: indexPath) as! UITableViewCell
+    
+    return cell
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
