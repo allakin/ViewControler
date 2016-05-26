c891bca27433705c0a6d117fb50e15690bec075d
Pavel <19paul91@mail.ru>
2016-05-27 00:17:00 +0300

    add files DetailsTableViewCell

---
+++ MyRestaurants/MyRestaurants/DetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsTableViewCell.swift
+//  MyRestaurants
+//
+//  Created by Павел Анплеенко on 27/05/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
