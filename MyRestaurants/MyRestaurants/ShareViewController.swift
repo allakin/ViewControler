f8c9fdc6cebadc5a0392afeefe658b1e41bd342f
Pavel <19paul91@mail.ru>
2016-06-02 20:51:46 +0300

    связал 4 иконки с view controller

--- MyRestaurants/MyRestaurants/ShareViewController.swift
+++ MyRestaurants/MyRestaurants/ShareViewController.swift
@@ -9,27 +9,36 @@
 import UIKit
 
 class ShareViewController: UIViewController {
+	
+	@IBOutlet weak var backgraundImageView: UIImageView!
+	@IBOutlet weak var topLeft: UIView!
+	@IBOutlet weak var topRight: UIView!
+	@IBOutlet weak var bottomRight: UIView!
+	@IBOutlet weak var bottomLeft: UIView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	override func viewDidLoad() {
+					super.viewDidLoad()
+		//изображение за пределами экрана
+		topLeft.transform = CGAffineTransformMakeTranslation(0, -500)
+		topRight.transform = CGAffineTransformMakeTranslation(0, -500)
+		topRight.transform = CGAffineTransformMakeTranslation(0, 500)
+		bottomLeft.transform = CGAffineTransformMakeTranslation(0, 500)
+	}
 
-        // Do any additional setup after loading the view.
-    }
+	override func didReceiveMemoryWarning() {
+					super.didReceiveMemoryWarning()
+					// Dispose of any resources that can be recreated.
+	}
+	
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	/*
+	// MARK: - Navigation
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+					// Get the new view controller using segue.destinationViewController.
+					// Pass the selected object to the new view controller.
+	}
+	*/
 
 }
